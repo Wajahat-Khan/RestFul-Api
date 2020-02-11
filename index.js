97678f403cef53344078d2a46e38bd33422f0456
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2020-02-11 16:13:30 +0500

    each course get api

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const express = require('express')
 
 const app=express()
-
+courses=[
+    {id:1,name:'Course1'},
+    {id:2,name:'Course2'},
+    {id:3,name:'Course3'}
+]
 app.get('/', (req,res)=>{
 res.send('Hello Listening')
 });
 
 app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3])
+    res.send(courses)
 })
 
-app.get('/api/courses/:year/:month',(req,res)=>{
-    res.send(req.query);
-    //res.send(req.params);
+app.get('/api/courses/:id',(req,res)=>{
+    const course=courses.find(c=>c.id===parseInt(req.params.id))
+    if(!course) res.status(400).send("Nothing found");
+    res.send(course)
 })
 
 const port = process.env.PORT || 3000;
