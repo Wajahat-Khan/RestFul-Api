c2708cc61dd3b5997bc6874faca29c992254e694
Wajahat Hussain <wajahat.hussain@northbaysolutions.net>
2020-02-11 15:54:51 +0500

    get api

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ app.get('/', (req,res)=>{
 res.send('Hello Listening')
 });
 
-app.listen(3000, ()=>{
-    console.log('Listening to port 3000')
+app.get('/api/courses',(req,res)=>{
+    res.send([1,2,3])
+})
+
+app.get('/api/courses/:year/:month',(req,res)=>{
+    res.send(req.query);
+    //res.send(req.params);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`Listening to port ${port}`)
 })
